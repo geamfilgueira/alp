9aebe7a1b28e24137150cc70e14949a6dbe36b61
geamfilgueira <geamfilgueira@gmail.com>
2022-08-11 10:16:26 -0300

    Commit

--- listas_monitores/q2.py
+++ listas_monitores/q2.py
@@ -1,19 +1,12 @@
-a = int(input())
-
-b = 0
+a = int(input("Digite um numero: "))
+b = 0 # inicializando com 0 para representar o primeiro par (quanto num positivo) ou o último par ( quando o num for negativo)
 
 if a > 0:
     while b < a:
-        print(b)
+        print("Par positivo: ", b)
         b = b + 2
 else:
-    if a % 2 == 0:
-        while a <= b:
-            print(a)
-            a = a + 2
-    else: 
+    while a <= b:
+        if a % 2 == 0:
+            print("Par negativo: ", a)
         a = a + 1
-        while a <= b:
-            print(a)
-            a = a + 2 
-
